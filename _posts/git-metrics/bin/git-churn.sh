4a9d56d86722b51a99fbfd87b19cc171edce3bbd
Andreas Heim <andreas@heim.no>
2012-04-14 21:53:26 +0200

    removed git-metrics

--- _posts/git-metrics/bin/git-churn.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-git log --all -M -C --name-only --format='format:' "$@" | sort | grep -v '^$' | uniq -c | sort | awk 'BEGIN {print "\{"} {print "\"" $2 "\"" "=>" "\"" $1 "\","} END {print "\}"}' | tr '\n' ' '
--- _posts/git-metrics/git-churn-and-graph.md
+++
@@ -1,1 +0,0 @@
-git log --all -M -C --name-only --format='format:' "$@" | sort | grep -v '^$' | uniq -c | sort | awk 'BEGIN {print "\{"} {print "\"" $2 "\"" "=>" "\"" $1 "\","} END {print "\}"}' | tr '\n' ' '
